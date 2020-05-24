0929e71ea4c0f1b9f309fcfae193dc3c06215879
Saurabh <emosrb@gmail.com>
2020-05-24 21:59:35 +0530

    202-204 creating and submiting the form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.signupForm = new FormGroup({
+         'username': new FormControl(null),
+        'email': new FormControl(null),
+        'gender': new FormControl('male')
+    });
+  }
+
+  onSubmit() {
+    console.log(this.signupForm);
+  }
+
 }
